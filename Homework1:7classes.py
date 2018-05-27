d064915b19b41fb6dfc7b16879182c295f5fa7e4
ppotashnik <39464964+ppotashnik@users.noreply.github.com>
2018-05-27 19:30:18 +0300

    Add files via upload

---
+++ Homework1:7classes.py
@@ -0,0 +1,56 @@
+class animals:
+    heart = 1
+    blood = True
+    eyes = 2
+    mouth = 1
+    nose = 1
+
+class mammals(animals):
+    legs = 4
+    birth = parturition
+    kids_food = milk
+    wings = False
+
+class birds(animals):
+    legs = 2
+    birth = egg
+    kids_food = worms
+    wings = True
+
+class cow(mammals):
+    chewing_gum = True
+    food = grass
+    speak = mu
+
+class goat(mammals):
+    chewing_gum = True
+    food = grass
+    speak = me
+    
+class sheep(mammals):
+    chewing_gum = True
+    food = grass
+    speak = be
+
+class pig(mammals):
+    chewing_gum = False
+    food = different_food
+    speak = hru
+
+class duck(bird):
+    swimming = True
+    beak = oblate
+    webbeds = True
+    speak = krya
+
+class chicken(bird):
+    swimming = False
+    beak = odinary
+    webbeds = True
+    speak = koko
+
+class goose(bird):
+    swimming = True
+    beak = oblate
+    webbeds = True
+    speak = gaga
